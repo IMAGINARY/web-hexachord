97883a51a9573b9a94edb398593b07d92f8e8935
Corentin Guichaoua <guichaoua@math.unistra.fr>
2019-05-29 17:19:20 +0200

    Exact computation for clock node radius

--- index.js
+++ index.js
@@ -298,6 +298,7 @@ let trichordTonnetz = {
         `
 };
 
+// FIXME: Don't prevent scroll when the view is locked
 // Slotted component that handles the drag and zoom logic on an svg
 let dragZoomSvg = {
     props: {
@@ -837,9 +838,15 @@ let chickenWire = {
 // Note component : a clickable circle with the note name
 let noteClock = {
     mixins: [activableMixin],
+    props:{
+        nodeRadius: {
+            type:Number,
+            default:24
+        }
+    },
     template: `
-        <g>
-            <circle v-bind:class="{activeNode:isActive}" r="24" v-bind:data-key="notes[0].text">
+        <g class=noteClock>
+            <circle v-bind:class="{activeNode:isActive}" :r="nodeRadius" v-bind:data-key="notes[0].text">
             </circle> 
             <text>
                 {{ notes[0].text }}
@@ -857,6 +864,10 @@ let clockOctave = {
     props: {
         height: Number,
         width: Number,
+        nodeRadius: {
+            type:Number,
+            default:24
+        },
         notes: Array,
         intervals: {
             type: Number,
@@ -868,8 +879,8 @@ let clockOctave = {
             return {y:this.height/2,x:this.width/2}
         },
         radius: function(){
-            //TODO: Cleaner computation
-            return Math.min(this.height/2,this.width/2)*0.85;
+            const nodeStrokeWidth = 2; //TODO: adjust according to the style sheet
+            return Math.min(this.height/2,this.width/2) - this.nodeRadius - nodeStrokeWidth/2;
         },
         viewbox: function(){
             return `0 0 ${this.width} ${this.height}`
@@ -887,10 +898,9 @@ let clockOctave = {
         points: function (){
             return this.getCoords.map( ({x,y}) => `${x},${y}` ).join(' ')
         },
-        //Returns true if any note is active
-        //TODO: Rename and repurpose as a count of active notes
-        anyNote: function (){
-            return this.notes.some(note => note.count>0);
+        //Returns the number of active notes
+        noteCount: function (){
+            return this.notes.reduce( (count,note) => (note.count>0 ? count+1 : count),0);
         }
     },
     methods: {
@@ -920,14 +930,14 @@ let clockOctave = {
             v-bind:width="width" v-bind:height="height" 
             v-bind:viewBox="viewbox">
             <circle v-bind:cx="center.x" v-bind:cy="center.y" v-bind:r="radius"/>
-            <polygon v-if="anyNote" class=clockPolygon
+            <polygon v-if="noteCount>1" class=clockPolygon
                 v-bind:points="points"/>
             <clickToPlayWrapper v-for="n in [0,1,2,3,4,5,6,7,8,9,10,11]" 
             :pitches="nodesToPitches([n])"
             v-bind:key="genKey([n])"
             :transform="posToTransform(position(n))" v-once>
                 <note-clock
-                v-bind:notes="node2Notes([n])"
+                v-bind:notes="node2Notes([n])" :nodeRadius="nodeRadius"
                 />
             </clickToPlayWrapper>
             
