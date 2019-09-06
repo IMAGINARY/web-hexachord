b2b1f8336b850a657c646c45591e352a4acdf185
Corentin Guichaoua <guichaoua@math.unistra.fr>
2019-09-06 15:00:09 +0200

    Fix for clockOctave using changing objects

--- js/components/clockOctave.js
+++ js/components/clockOctave.js
@@ -94,7 +94,7 @@ let clockOctave = {
             <clickToPlayWrapper v-for="n in [0,1,2,3,4,5,6,7,8,9,10,11]" 
             :pitches="nodesToPitches([n])"
             v-bind:key="genKey([n])"
-            :transform="posToTransform(position(n))" v-once>
+            :transform="posToTransform(position(n))">
                 <note-clock
                 v-bind:notes="node2Notes([n])"
                 />
